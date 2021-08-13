1fda87f306c66a5f123389fc036a77330071f017
akshay0cr7 <akshayharish1425@gmail.com>
2021-08-13 15:23:26 -0700

    first commit

---
+++ portfolio.js
@@ -0,0 +1,36 @@
+//  $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY>20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+//     })
+//  });
+
+// $(document).ready(function(){
+//     $("#link").click(function(e){
+//         e.preventDefault();
+//         window.location.href="AKSHAY_CV.pdf";
+//     })
+// });
+// $(document).ready(function(){
+//     $("#facebook").click(function(e){
+//         e.preventDefault();
+//         window.location.href="https://www.facebook.com/profile.php?id=100065127665920";
+//     })
+// });
+// $(document).ready(function(){
+//     $("#linkedin").click(function(e){
+//         e.preventDefault();
+//         window.location.href="https://www.linkedin.com/in/akshay-harish-298665212/";
+//     })
+// });
+
+// $(document).ready(function(){
+//     $("#instagram").click(function(e){
+//         e.preventDefault();
+//         window.location.href="https://www.instagram.com/akshay.goku2/";
+//     })    
+// });
+
